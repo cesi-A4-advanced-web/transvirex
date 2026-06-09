bc57740d2b532ecccd99fac56850462c61636b04
AlecFoucher <115996644+AlecFoucher@users.noreply.github.com>
2026-06-09 15:12:33 +0200

    feat: implement JWT authentication and user fetching in frontend

--- packages/backend/apps/gateway/src/gateway.controller.ts
+++ packages/backend/apps/gateway/src/gateway.controller.ts
@@ -91,6 +91,11 @@ export class GatewayController {
         return this.gatewayService.executePostgreSQL(query);
     }
 
+    @Get('auth/me')
+    me(@Req() req: Request) {
+        return (req as any).user;
+    }
+
     @Public()
     @Post('auth/login')
     async login(
